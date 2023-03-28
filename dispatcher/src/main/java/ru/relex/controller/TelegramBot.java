fa0f4d84c54b4a00d9f6b4303dda08c4472ae762
Bondarina <darinbondd@gmail.com>
2023-03-28 10:24:15 +0300

    test

--- dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -84,7 +84,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     public void startCommandReceived(Message message, long chatId, String name) {
-        String answer = name + ", как настроение? Солнечное? (нажми внизу кнопку Да/Нет)\n\n";
+        String answer = name + ", как настроение? Солнечное? (нажми внизу кнопку Да/Не знаю)\n\n";
 
         sendMsg(message, chatId, answer);
     }
